200bc8186182f049e4b5b7069a4bce5c4e8316e1
Peter Peerdeman <peterpeerdeman@gmail.com>
2024-08-25 23:46:05 +0200

    feat(nvim): upgrade harpoon, reorder modules

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -1,8 +1,13 @@
-local mark = require("harpoon.mark")
-local ui = require("harpoon.ui")
+local harpoon = require("harpoon")
 
-vim.keymap.set("n", "<leader>a", mark.add_file)
-vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
-vim.keymap.set("n", "<leader>j", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<leader>k", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<leader>l", function() ui.nav_file(3) end)
+harpoon:setup()
+
+vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+vim.keymap.set("n", "<leader>j", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<leader>k", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<leader>l", function() harpoon:list():select(3) end)
+
+-- Toggle previous & next buffers stored within Harpoon list
+vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
--- nvim/lua/pp/packer.lua
+++ nvim/lua/pp/packer.lua
@@ -13,23 +13,31 @@ return require('packer').startup(function(use)
 	use ({
 		'nvim-treesitter/nvim-treesitter',
 		run = ':TSUpdate'
-	})
-	use ('theprimeagen/harpoon')
-	use ('mbbill/undotree')
-	use ({'VonHeikemen/lsp-zero.nvim', branch = 'v4.x'})
-	use ('neovim/nvim-lspconfig')
-	use ('hrsh7th/cmp-nvim-lsp')
-	use ('hrsh7th/nvim-cmp')
+    })
+    use {
+        "ThePrimeagen/harpoon",
+        branch = "harpoon2",
+        requires = { {"nvim-lua/plenary.nvim"} }
+    }
+    use ('mbbill/undotree')
+    use ('folke/zen-mode.nvim')
+
+    use ({'VonHeikemen/lsp-zero.nvim', branch = 'v4.x'})
+    use ('williamboman/mason.nvim')
+    use ('williamboman/mason-lspconfig.nvim')
+    use ('neovim/nvim-lspconfig')
+    use ('hrsh7th/cmp-nvim-lsp')
+    use ('hrsh7th/nvim-cmp')
 
     -- classics
-	use ('tpope/vim-fugitive')
-	use ('tpope/vim-surround')
-	use ('tpope/vim-unimpaired')
-	use ('mg979/vim-visual-multi')
+    use ('tpope/vim-fugitive')
+    use ('tpope/vim-surround')
+    use ('tpope/vim-unimpaired')
+    use ('mg979/vim-visual-multi')
 
     -- colorings
-	use({ 'folke/tokyonight.nvim' })
-	vim.cmd('colorscheme tokyonight-moon')
-	-- vim.cmd('colorscheme tokyonight-day')
+    use({ 'folke/tokyonight.nvim' })
+    vim.cmd('colorscheme tokyonight-moon')
+    -- vim.cmd('colorscheme tokyonight-day')
 
 end)
