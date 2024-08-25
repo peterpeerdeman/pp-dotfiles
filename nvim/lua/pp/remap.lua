4ae88e91047e2e175dfca2a4d9664606622b8fb0
Peter Peerdeman <peterpeerdeman@gmail.com>
2024-08-25 22:02:20 +0200

    feat(nvim): re-add my mappings

--- nvim/lua/pp/remap.lua
+++ nvim/lua/pp/remap.lua
@@ -1,2 +1,17 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Custom: Redo
+vim.keymap.set("n", "U", vim.cmd.redo)
+
+-- Custom: Easier buffer switching
+vim.keymap.set("n", "<C-h>", "<C-W>h")
+vim.keymap.set("n", "<C-j>", "<C-W>j")
+vim.keymap.set("n", "<C-k>", "<C-W>k")
+vim.keymap.set("n", "<C-l>", "<C-W>l")
+
+-- Custom: Resize panes
+vim.keymap.set("n", "<leader><Up>", function() vim.cmd [[resize +1]] end)
+vim.keymap.set("n", "<leader><Down>", function() vim.cmd [[resize -1]] end)
+vim.keymap.set("n", "<leader><Right>", function() vim.cmd [[vertical resize +5]] end)
+vim.keymap.set("n", "<leader><Left>", function() vim.cmd [[vertical resize -5]] end)
