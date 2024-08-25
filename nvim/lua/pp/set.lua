d2ee0e5e20c7cc6e4ad11688eed891c048ad8cfe
Peter Peerdeman <peterpeerdeman@gmail.com>
2024-08-25 22:12:05 +0200

    feat(set): update undodir

--- nvim/lua/pp/set.lua
+++ nvim/lua/pp/set.lua
@@ -14,7 +14,7 @@ vim.opt.wrap = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.nvim/undodir"
+vim.opt.undodir = os.getenv("HOME") .. "/.config/nvim/undodir"
 vim.opt.undofile = true
 
 vim.opt.hlsearch = false
