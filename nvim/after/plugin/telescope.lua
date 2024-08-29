6a97c2f73d41357eecbec1734695ad8078f4ac69
Peter Peerdeman <peterpeerdeman@gmail.com>
2024-08-29 14:08:20 +0200

    feat(nvim): tweak remaps

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,7 +1,18 @@
 local builtin = require('telescope.builtin')
+
+-- primeagen archive
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>ps', function()
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-	builtin.grep_string({ search = vim.fn.input("Grep > ") })
+    builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
+
+-- primeagen
+--vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+
+-- telescope originals
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
