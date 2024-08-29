3b2ca703b927e12a0128d1811da173c77ab9bd8e
Peter Peerdeman <peterpeerdeman@gmail.com>
2024-08-29 12:18:17 +0200

    fix(nvim): fix default yank to clipboard

--- nvim/lua/pp/set.lua
+++ nvim/lua/pp/set.lua
@@ -30,6 +30,6 @@ vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
 
-vim.opt.clipboard = unnamedplus
+vim.opt.clipboard = "unnamedplus"
 -- vim.opt.colorcolumn = "80"
 vim.opt.splitright = true
